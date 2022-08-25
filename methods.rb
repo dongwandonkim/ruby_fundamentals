bb98a24ede10654a385550661f3ad57614aa59e8
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-25 00:13:21 -0700

    methods

---
+++ methods.rb
@@ -0,0 +1,19 @@
+def multiply(number1, number2)
+  number1.to_f * number2.to_f
+end
+
+def divide(number1, number2)
+  number1.to_f / number2.to_f
+end
+
+def add(number1, number2)
+  number1.to_f + number2.to_f
+end
+
+def subtract(number1, number2)
+  number1.to_f - number2.to_f
+end
+
+def mod(number1, number2)
+  number1.to_f % number2.to_f
+end
