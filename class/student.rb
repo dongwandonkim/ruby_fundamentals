f68548fd203a87785dbf44deadb94b9a817b948e
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-08-26 22:14:06 -0700

    class getter setter

---
+++ class/student.rb
@@ -0,0 +1,48 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  # @first_name
+  # @last_name
+  # @email
+  # @username
+  # @password
+
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
+
+  # def first_name=(first_name)
+  #   @first_name = first_name
+  # end
+
+  # def first_name
+  #   @first_name
+  # end
+
+  # def last_name=(last_name)
+  #   @last_name = last_name
+  # end
+
+  # def last_name
+  #   @last_name
+  # end
+
+end
+
+don = Student.new("Don", "Hernandez", "test@test.com", "don", "pass123")
+# don.first_name = "Don"
+# don.last_name = "Kim"
+# don.email = "test5@test.com"
+# don.username = "donkim"
+
+p don.first_name
+p don.last_name
+p don.email
+p don.username
+p don.password
+
+p don
